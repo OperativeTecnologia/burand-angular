b4755a44ba38bbac13815cedc72b4970868cbf52
Leonardo Tonizza <leonardo.tonizza@hotmail.com>
2023-04-10 22:58:30 -0300

    refactor: onlyDigits to removeNonDigits

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,6 +3,6 @@ export * from './base64ToFile';
 export * from './calculateAge';
 export * from './inputUUID';
 export * from './LocalStorage';
-export * from './onlyDigits';
 export * from './readFileAsDataURL';
 export * from './removeAccents';
+export * from './removeNonDigits';
--- src/utils/onlyDigits.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Remove os acentos de uma string.
- *
- * @param str A string a ser tratada.
- * @returns A string sem acentos.
- */
-export function onlyDigits(text: string): string {
-  return text.replace(/\D/g, '');
-}
