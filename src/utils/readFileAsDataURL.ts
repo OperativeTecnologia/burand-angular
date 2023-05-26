a9d8a0f8630e408c99c5a8b719a12844c61a06cb
Leonardo Tonizza <leonardo.tonizza@hotmail.com>
2023-05-26 11:16:46 -0300

    fix(utils): typing

--- src/utils/readFileAsDataURL.ts
+++ src/utils/readFileAsDataURL.ts
@@ -2,13 +2,13 @@
  * Lê um `File` e retorna seus dados no formato de URL Data.
  *
  * @param file - O arquivo a ser lido.
- * @returns Uma `Promise` que resolve em uma string contendo a URL Data do arquivo ou `null`.
+ * @returns Uma `Promise` que resolve em uma `string` contendo a URL Data do arquivo ou `null`.
  */
-export function readFileAsDataURL(file: File): Promise<string | ArrayBuffer | null> {
+export function readFileAsDataURL(file: File): Promise<string | null> {
   return new Promise((resolve, reject) => {
     const fileReader = new FileReader();
     fileReader.onload = (): void => {
-      resolve(fileReader.result);
+      resolve(fileReader.result as string | null);
     };
     fileReader.onerror = (): void => {
       reject();
